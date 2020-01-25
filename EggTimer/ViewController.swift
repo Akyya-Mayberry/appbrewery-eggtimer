3965e478dc0eda78225af8a3a91aa03e6e3f1248
hollywoodno <hollywoodno@gmail.com>
2020-01-25 15:41:42 -0800

    finish basic assignments

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  let eggCookingTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+  var count = 0
+  var cookTime = 0
+  var secondsPassed = 0
+  var cookingTimer: Timer?
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var progressView: UIProgressView!
+  
+  // MARK: - Methods
+  
+  @IBAction func hardnessSelected(_ sender: UIButton) {
     
-
-
+    cookingTimer?.invalidate()
+    progressView.progress = 0.0
+    secondsPassed = 0
+    
+    if let hardness = sender.currentTitle {
+      if let eggTime = eggCookingTimes[hardness] {
+        titleLabel.text = hardness
+        cookTime = eggTime
+        
+        cookingTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+      }
+    }
+    
+  }
+  
+  @objc func updateTimer() {
+    if secondsPassed < cookTime   {
+      secondsPassed += 1
+      
+      let percentageComplete = Float(secondsPassed) / Float(cookTime)
+      progressView.progress = percentageComplete
+    } else {
+      titleLabel.text = "DONE!"
+    }
+  }
 }
